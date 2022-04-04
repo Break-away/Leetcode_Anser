d1e09eca2558fa375d8c7a26be314fdf6542fe2b
breakaway <rainbow19940209@outlook.com>
2022-04-04 21:39:05 +0800

    leetcode 123

--- maxProfit.cpp
+++ maxProfit.cpp
@@ -102,4 +102,67 @@ public:
         }
         return total;
     }
+
+    // 123. 买卖股票的最佳时机 III
+    // 给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。
+    // 设计一个算法来计算你所能获取的最大利润。你最多可以完成 两笔 交易。
+    // 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+
+    //错误解答
+    static bool compare(int a, int b){
+        return a>b;
+    }
+    int maxProfit(vector<int>& prices) {
+        int total=0;
+        vector<int> profitvec;
+        int buy=prices[0], sell=prices[0];
+        for (int i=1; i<=prices.size(); ++i)
+        {
+            int profit=0;
+            //核查股票收益是否已经结算
+            if (i == prices.size()){
+                //如果sell和buy价格不相等则未结算
+                if (sell != buy) {
+                    profit == sell-buy;
+                    profitvec.push_back(profit);
+                }
+            }
+            //如果价格开始下降，则有两种情况：
+            else if (prices[i] <= sell){
+                //第一种情况：股票价格一直下跌，则将buy和sell价格调整为较低价格
+                if (sell == buy){
+                    sell=prices[i];
+                    buy=prices[i];
+                }
+                //第二种情况：股票先上涨，后下降，则将此前的收益兑现，
+                //并将buy和sell价格调整为今日价格
+                else{
+                    profit == sell-buy;
+                    profitvec.push_back(profit);
+                    sell=prices[i];
+                    buy=prices[i];
+                }
+            }
+            //如果价格开始上涨，则将sell更新；
+            else{
+                sell = prices[i];
+            }
+        }
+        std::sort(profitvec.begin(),profitvec.end(),compare);
+        return profitvec[0]+profitvec[1];
+    }
+
+    //状态转移
+    int maxProfit(vector<int>& prices){
+        int buy1=-prices[0], sell1=0;
+        int buy2=-prices[0], sell2=0;
+        for (int i=0; i<prices.size(); ++i)
+        {
+            buy1 = max(buy1,-prices[i]);
+            sell1 = max(sell1,buy1+prices[i]);
+            buy2 = max(buy2,sell1-prices[i]);
+            sell2 = max(sell2, buy2+prices[i]);
+        }
+        return sell2;
+    }
 };
