3ac43aae74aba88e88e1452304cf588109c87b8d
breakaway <rainbow19940209@outlook.com>
2022-04-04 14:48:57 +0800

    leetcode 119

--- generate.cpp
+++ generate.cpp
@@ -29,4 +29,26 @@ public:
         }
         return ans;
     }
+    // 119. 杨辉三角 II
+    // 给定一个非负索引 rowIndex，返回「杨辉三角」的第 rowIndex 行。
+    // 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+    vector<int> getRow(int rowIndex) {
+        vector<vector<int>> ans;
+        if (rowIndex == 0) return {1};
+        if (rowIndex == 1) return {1,1};
+        ans.push_back({1});
+        ans.push_back({1,1});
+        for (int i=2; i<=rowIndex; i++){
+            ans.push_back({});
+            for (int j=0; j<=i; j++){
+                if (j == 0 || j ==i ) {
+                    ans[i].push_back(1);
+                }
+                else{
+                    ans[i].push_back(ans[i-1][j-1]+ans[i-1][j]);
+                }
+            }
+        }
+        return ans[rowIndex];
+    }
 };
