4ea8df80a73b7ef2396965d34dfcf82f121f6cb9
breakaway <rainbow19940209@outlook.com>
2022-04-04 20:54:14 +0800

    leetcode 122

--- maxProfit.cpp
+++ maxProfit.cpp
@@ -26,6 +26,7 @@ public:
     //     return maximum;
     // }
 
+    //121
     int maxProfit(vector<int>& prices){
         int minprice=INT_MAX, maxProfit=0;
         for (auto price : prices){
@@ -33,5 +34,72 @@ public:
             minprice = min(minprice,price);
         }
         return maxProfit;
+    }
+
+    // 122. 买卖股票的最佳时机 II
+    // 给定一个数组 prices ，其中 prices[i] 表示股票第 i 天的价格。
+    // 在每一天，你可能会决定购买和/或出售股票。你在任何时候 最多 
+    // 只能持有 一股 股票。你也可以购买它，然后在 同一天 出售。
+    // 返回 你能获得的 最大 利润 。
+
+    //自解
+    int maxProfit(vector<int>& prices) {
+        int total=0;
+        int buy=prices[0], sell=prices[0];
+        for (int i=1; i<=prices.size(); ++i)
+        {
+            //核查股票收益是否已经结算
+            if (i == prices.size()){
+                //如果sell和buy价格不相等则未结算
+                if (sell != buy) total += sell-buy;
+            }
+            //如果价格开始下降，则有两种情况：
+            else if (prices[i] <= sell){
+                //第一种情况：股票价格一直下跌，则将buy和sell价格调整为较低价格
+                if (sell == buy){
+                    sell=prices[i];
+                    buy=prices[i];
+                }
+                //第二种情况：股票先上涨，后下降，则将此前的收益兑现，
+                //并将buy和sell价格调整为今日价格
+                else{
+                    total += sell-buy;
+                    sell=prices[i];
+                    buy=prices[i];
+                }
+            }
+            //如果价格开始上涨，则将sell更新；
+            else{
+                sell = prices[i];
+            }
+        }
+        return total;
+    }
+
+    //动态规划
+    int maxProfit(vector<int>& prices){
+        //dp0表示当天交易完成后手里没有股票的利润；
+        //dp1表示当天交易完成后手里有股票的利润；
+        int n=prices.size();
+        int dp0 = 0;
+        int dp1 = -prices[0];
+        for (int i=1; i<n; ++i){
+            int newdp0 = max(dp0,dp1+prices[i]);
+            int newdp1 = max(dp1,dp0-prices[i]);
+            dp0 = newdp0;
+            dp1 = newdp1;
+        }
+        return dp0;
+    }
+
+    //贪心算法
+    int maxProfit(vector<int>& prices){
+        int total=0;
+        int n=prices.size();
+        for (int i=1; i<n; ++i){
+            //如果当天价格比前一天价格高，则默认前一天已购买股票，今天卖出
+            total += max(0,prices[i]-prices[i-1]);
+        }
+        return total;
     }
 };
